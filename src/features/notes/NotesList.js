a4c2ced0e7b945e96a39914bb729cd1b0e675eaf
Athul <raoathul@gmail.com>
2022-12-31 19:33:09 +0530

    Patch

--- src/features/notes/NotesList.js
+++ src/features/notes/NotesList.js
@@ -35,7 +35,7 @@ const NotesList = () => {
       filteredIds = ids.filter((noteId) => entities[noteId].username === username);
     }
 
-    const tableContent = ids?.length && ids.map((noteId) => <Note key={noteId} noteId={noteId} />);
+    const tableContent = ids?.length && filteredIds.map((noteId) => <Note key={noteId} noteId={noteId} />);
 
     content = (
       <table className="table table--notes">
